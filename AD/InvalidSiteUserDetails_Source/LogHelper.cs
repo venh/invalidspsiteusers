bb7fca93b9a2819b6aee669b6f4c84e55438aa49
Venkatesh Subramanian <subramanian_v@India.XoriantCorp.com>
2018-02-16 20:55:50 +0530

    initial commit

---
+++ AD/InvalidSiteUserDetails_Source/LogHelper.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Configuration;
+
+namespace InvalidSiteUserDetails
+{
+    class LogHelper
+    {
+        const int splitNum = 1; // the num of  key, use to split column like tab
+        const char splitChar = ','; // tab key,use to split column
+        static string strSplitter = string.Empty;
+
+        public static void LogUserDetails(string usrId, string usrFullName, string usrLoginName, string usrEmail)
+        {
+            string fPath = Path.Combine(Directory.GetCurrentDirectory(), ConfigurationManager.AppSettings["UserDetailLogName"]);
+            strSplitter = ConfigurationManager.AppSettings["Splitter"];
+            string[] arr = new string[] { usrId, usrFullName, usrLoginName, usrEmail };
+            string strLogContent = string.Join(strSplitter, arr);
+            string strFileHeader = ConfigurationManager.AppSettings["UserHeader"];
+            if (DoesHeaderExist(fPath, strFileHeader))
+                strLogContent = FormatLog(strLogContent, false, strFileHeader);
+            else
+                //strLogContent = FormatLog(strFileHeader, true, strFileHeader) + "\r\n" + FormatLog(strLogContent, false, strFileHeader);
+                strLogContent = FormatLog(strFileHeader, true, strFileHeader) + "\r\n" + strLogContent;
+
+            using (StreamWriter sw = new StreamWriter(fPath, true))
+            {
+                sw.WriteLine(strLogContent);
+                sw.Flush();
+                sw.Close();
+            }
+        }
+
+        static string FormatLog(string inputStr, bool isHeader, string fileHeader)
+        {
+            string outputStr = string.Empty;
+            string[] str = inputStr.Split('|');
+            string[] header = fileHeader.Split(strSplitter.ToCharArray());
+            string splitTab = GetSplitStr(splitNum, strSplitter.ToCharArray()[0]);
+            StringBuilder sbOp = new StringBuilder(str.Length);
+            for (int j = 0; j < str.Length; j++)
+            {
+                if (!isHeader)
+                {
+                    if (str[j].Length < header[j].Length)
+                        str[j] = str[j].PadRight(header[j].Length);
+                }
+                sbOp.Append(str[j]);
+                sbOp.Append(splitTab);
+            }
+            return sbOp.ToString().TrimEnd(splitChar);
+        }
+
+        static string GetSplitStr(int Num, char splitChar)
+        {
+            StringBuilder sbSplitTab = new StringBuilder(Num);
+            for (int i = 1; i <= Num; i++)
+            {
+                sbSplitTab.Append(splitChar);
+            }
+            return sbSplitTab.ToString();
+        }
+
+        static bool DoesHeaderExist(string filePath, string fileHeader)
+        {
+            bool flag = false;
+            try
+            {
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    string headerLine = reader.ReadLine();
+                    string[] arrFileHeader = fileHeader.Split('|');
+                    if (string.IsNullOrEmpty(headerLine))
+                    {
+                        flag = false;
+                        reader.Close();
+                        return flag;
+                    }
+                    string[] arrHeaderLine = headerLine.Split(strSplitter.ToCharArray());
+                    if (arrFileHeader[0] == arrHeaderLine[0])
+                        flag = true;
+                    else
+                        flag = false;
+                    reader.Close();
+                }
+            }
+            catch (Exception)
+            {
+                flag = false;
+            }
+            return flag;
+        }
+    }
+}
---
+++ WS/InvalidSiteUserDetails_Source/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Xml;
+using System.Net;
+using System.Linq;
+using System.Xml.Linq;
+using System.Collections;
+using System.Configuration;
+using System.Collections.Generic;
+using InvalidSiteUserDetails.UserGroupProxy;
+
+namespace InvalidSiteUserDetails
+{
+    class Program
+    {
+        static string SITE_URL = string.Empty;
+        static string USER_NAME = string.Empty;
+        static string PASSWORD = string.Empty;
+        static string DOMAIN = string.Empty;        
+
+        static void Main(string[] args)
+        {
+            string accName = string.Empty;
+            string userName = string.Empty;
+            string usrIdName = string.Empty;
+            UserGroup ugProxy = null;
+            XmlNode usrsNode = null;
+            XElement usrDoc = null;
+            IEnumerable<XElement> userEles = null;
+            string DASHED_LINE = "......................................................";
+            string userDomain = string.Empty;
+            ConsoleKeyInfo key;
+            try
+            {
+                SITE_URL = ConfigurationManager.AppSettings["SITE_URL"];
+                Console.Write("Please enter a valid user name: ");
+                USER_NAME = Console.ReadLine();
+                Console.Write("Please enter the password: ");
+                do
+                {
+                    key = Console.ReadKey(true);
+                    // Backspace Should Not Work
+                    if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
+                    {
+                        PASSWORD = string.Concat(PASSWORD, key.KeyChar);
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        if (key.Key == ConsoleKey.Backspace && PASSWORD.Length > 0)
+                        {
+                            PASSWORD = PASSWORD.Substring(0, (PASSWORD.Length - 1));
+                            Console.Write("\b \b");
+                        }
+                    }
+                }
+                // Stops Receving Keys Once Enter is Pressed
+                while (key.Key != ConsoleKey.Enter);
+                Console.WriteLine();
+                Console.Write("Please enter a valid domain: ");
+                DOMAIN = Console.ReadLine();
+                Console.WriteLine("Starting to get Invalid Users from the site.");
+                ugProxy = new UserGroup();
+                ugProxy.Credentials = new NetworkCredential(USER_NAME, PASSWORD, DOMAIN);
+                ugProxy.Url = SITE_URL + "_vti_bin/usergroup.asmx";
+                usrsNode = ugProxy.GetAllUserCollectionFromWeb();
+                usrDoc = XElement.Parse(usrsNode.InnerXml);
+                userEles = usrDoc.Descendants();                
+                Console.WriteLine("Total User Count: " + userEles.Count());
+                Console.WriteLine(DASHED_LINE);
+                int failedCnt = 0;
+                foreach (var userEle in userEles)
+                {
+                    usrIdName = userEle.Attribute(XName.Get("ID")).Value + ";#" + userEle.Attribute(XName.Get("Name")).Value;
+                    accName = userEle.Attribute(XName.Get("LoginName")).Value;                   
+                    userName = accName.Substring(accName.LastIndexOf('\\') + 1);
+                    userDomain = accName.Replace("\\" + userName, string.Empty);
+                    if (userDomain.ToUpperInvariant() == DOMAIN.ToUpperInvariant())
+                    {
+                        bool bln = DoesUserExist(userName);
+                        if (!bln)
+                        {
+                            LogHelper.LogUserDetails(userName, userEle.Attribute("Name").Value, accName, userEle.Attribute("Email").Value);
+                            Console.WriteLine("UserName: " + userName + " : IsValid: " + bln.ToString());
+                            failedCnt++;
+                        }
+                    }
+                }
+                Console.WriteLine(DASHED_LINE);
+                Console.WriteLine("Invalid user count: " + failedCnt);
+                Console.WriteLine("Completed getting Invalid Users from the site.");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.ReadLine();
+            }
+            finally
+            {
+                if (ugProxy != null)
+                {
+                    ugProxy.Dispose();
+                    ugProxy = null;
+                }
+                usrsNode = null;
+                usrDoc = null;
+                userEles = null;
+            }
+        }
+
+        private static bool DoesUserExist(string strUser)
+        {
+            PeopleProxy.People pplProxy = null;
+            bool bUsrResolved = false;
+            PeopleProxy.PrincipalInfo[] pInfoArr = null;
+            try
+            {
+                pplProxy = new PeopleProxy.People();
+                pplProxy.Url = SITE_URL + "/_vti_bin/people.asmx";
+                pplProxy.Credentials = new NetworkCredential(USER_NAME, PASSWORD, DOMAIN);
+                pInfoArr = pplProxy.ResolvePrincipals(new string[] { strUser }, PeopleProxy.SPPrincipalType.User, false);
+                if ((pInfoArr != null) && (pInfoArr.Length > 0))
+                {
+                    bUsrResolved = pInfoArr[0].IsResolved;
+                }
+                return bUsrResolved;
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                pInfoArr = null;
+                if (pplProxy != null)
+                {
+                    pplProxy.Dispose();
+                    pplProxy = null;
+                }
+            }
+        }
+        
+        private static XmlNode GetXmlNode(XElement ele)
+        {
+            XmlDocument xD = new XmlDocument();
+            xD.LoadXml(ele.ToString());
+            XmlNode xN = xD.FirstChild;
+            return xN;
+        }
+    }
+}
